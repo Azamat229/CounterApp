9ebb37ebaa8d0e9fcd3ce2826f0cc2cfacfd69f5
azamat <azamatmambetaliev229@gmail.com>
2021-02-05 09:27:17 +0600

    first commit

---
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.counterapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    Button btn;
+    TextView txv;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        txv = findViewById(R.id.textView);
+//        @Override
+//        public void onSaveInstanceState(Bundle savedInstanceState) {
+//
+//        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+
+                String formatted = String.format("%04d", counter);
+                txv.setText((formatted));
+            }
+        });
+
+        if(savedInstanceState != null){
+            counter = savedInstanceState.getInt("count");
+            txv.setText(String.valueOf(counter  ));
+        }
+
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("count", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
